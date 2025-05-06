828097cd1facf57421e2ea0722f0a3333cd0cdbb
Mored <matheusmarques1902@gmail.com>
2025-05-05 22:50:06 -0300

    ajustando s

--- src/components/ResultMessage.tsx
+++ src/components/ResultMessage.tsx
@@ -49,7 +49,7 @@ const ResultMessage: React.FC<ResultMessageProps> = ({
   if (!phoneNumber.startsWith('55')) {
     phoneNumber = `55${phoneNumber}`;
   }
-  const encodedMessage = encodeURIComponent(`Olá! Estou buscando atendimento urgente. Sou cliente do Advogou.com`);s
+  const encodedMessage = encodeURIComponent(`Olá! Estou buscando atendimento urgente. Sou cliente do Advogou.com`);
   const whatsappUrl = `https://wa.me/${phoneNumber}?text=${encodedMessage}`;
   
   const copyToClipboard = () => {
